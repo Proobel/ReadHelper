ec50000e4be0d6bf24b6390415faead99a524d1d
Proobel <Geroi_6@bk.ru>
2018-10-15 17:52:46 +0300

    Hiding in tray created. Added tray icon.

---
+++ ReadHelper/ReadBookHelper.cs
@@ -0,0 +1,47 @@
+﻿using ClipboardHelper;
+using System;
+using System.Windows.Forms;
+
+namespace ReadHelper
+{
+    public partial class ReadBookHelper : Form
+    {
+        public static Translator Trans = new Translator();
+
+        public ReadBookHelper()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TrayIcon.Icon = Properties.Resources.BookIcon;
+            WindowState = FormWindowState.Minimized;
+
+            ClipboardMonitor.OnClipboardChange += Clip;
+            ClipboardMonitor.Start();
+            Trans.OnTranslateChange += () => { TranslatedTextLabel.BeginInvoke((MethodInvoker)(()=> TranslatedTextLabel.Text = Trans.TranslatedText));  };
+        }
+
+        public static void Clip(ClipboardFormat clipboard, object data)
+        {
+            Trans.TranslateByYandex(data as string);
+        }
+
+        private void ReadBookHelper_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                ShowInTaskbar = false;
+                TrayIcon.Visible = true;
+            }
+        }
+
+        private void TrayIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            TrayIcon.Visible = false;
+            ShowInTaskbar = true;
+            WindowState = FormWindowState.Normal;
+        }
+    }
+}
