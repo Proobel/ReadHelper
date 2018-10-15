d6ee6b163cd2ede446fcbf87062e18cb05fde53a
Proobel <Geroi_6@bk.ru>
2018-10-15 16:57:46 +0300

    Wrong Commit

--- ReadHelper/ReadHelperForm.Designer.cs
+++ ReadHelper/ReadHelperForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ReadHelper
 {
-    partial class Form1
+    partial class ReadHelperForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
+            this.TrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.SuspendLayout();
             // 
             // label1
@@ -41,15 +43,21 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
-            // Form1
+            // TrayIcon
+            // 
+            this.TrayIcon.Text = "Proobel Переводчик";
+            this.TrayIcon.Visible = true;
+            // 
+            // ReadHelperForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(650, 386);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.Name = "ReadHelperForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.ReadHelperForm_Resize);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,6 +66,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon TrayIcon;
     }
 }
 
--- ReadHelper/ReadHelperForm.cs
+++ ReadHelper/ReadHelperForm.cs
@@ -4,25 +4,34 @@ using System.Windows.Forms;
 
 namespace ReadHelper
 {
-    public partial class Form1 : Form
+    public partial class ReadHelperForm : Form
     {
         public static Translator Trans = new Translator();
 
-        public Form1()
+        public ReadHelperForm()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            TrayIcon.Icon = TranslateEye;
+            TrayIcon.Visible = false;
+
             ClipboardMonitor.OnClipboardChange += Clip;
             ClipboardMonitor.Start();
             Trans.OnTranslateChange += () => { label1.BeginInvoke((MethodInvoker)(()=> label1.Text = Trans.TranslatedText));  };
+
         }
 
         public static void Clip(ClipboardFormat clipboard, object data)
         {
             Trans.TranslateByYandex(data as string);
+        }
+
+        private void ReadHelperForm_Resize(object sender, EventArgs e)
+        {
+
         }
     }
 }
